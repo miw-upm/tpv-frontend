f18c3b6883fe1dc428c02c302622029effa34899
JBernal <j.bernal@upm.es>
2025-04-03 19:53:27 +0200

    removed interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,14 +4,13 @@ import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 
 import {routes} from './app.routes';
-import {tokenInterceptor} from '@core/interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({eventCoalescing: true}),
         provideAnimationsAsync(),
 
-        provideHttpClient(withInterceptors([tokenInterceptor])),
+        provideHttpClient(),
         provideRouter(routes)
     ]
 };
