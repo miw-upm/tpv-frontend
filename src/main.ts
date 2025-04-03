35ffd5a796ff3e6bdd135dc21e047f0526c7e9d7
JBernal <j.bernal@upm.es>
2025-04-03 20:05:34 +0200

    added spa-client-id

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,37 @@
 import {bootstrapApplication} from '@angular/platform-browser';
-import {appConfig} from './app/app.config';
 import {AppComponent} from './app/app.component';
+import {AuthConfig, OAuthModule, OAuthService} from "angular-oauth2-oidc";
+import {importProvidersFrom, provideZoneChangeDetection} from "@angular/core";
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
+import {provideHttpClient} from "@angular/common/http";
+import {provideRouter} from "@angular/router";
+import {routes} from "./app/app.routes";
 
-bootstrapApplication(AppComponent, appConfig)
-    .catch((err) => console.error(err));
+export const authConfig: AuthConfig = {
+    issuer: 'http://localhost:8080/tpv-user',
+    redirectUri: window.location.origin,
+    clientId: 'spa-client-id',
+    responseType: 'code',
+    scope: 'admin manager operator customer',
+    showDebugInformation: true,
+};
+
+(async () => {
+    const appRef = await bootstrapApplication(AppComponent, {
+        providers: [
+            provideZoneChangeDetection({eventCoalescing: true}),
+            provideAnimationsAsync(),
+            provideHttpClient(),
+            importProvidersFrom(OAuthModule.forRoot()),
+            provideRouter(routes)
+        ]
+    });
+
+    // Obtiene el servicio de OAuth desde el inyector de la aplicación
+    const injector = appRef.injector;
+    const oauthService = injector.get(OAuthService);
+
+    // Configura y carga el documento de descubrimiento, intentando el login
+    oauthService.configure(authConfig);
+    await oauthService.loadDiscoveryDocumentAndTryLogin();
+})();
